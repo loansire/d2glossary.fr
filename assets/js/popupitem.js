105395f931621d9d31517709e71b799da4dcb740
loansire <loan.sire@corys.fr>
2025-05-02 03:56:40 +0200

    adding icons in descriptions

--- assets/js/popupitem.js
+++ assets/js/popupitem.js
@@ -1,9 +1,45 @@
+function parseDescription(text) {
+  const replacements = {
+    '\\[Solaire\\]': '<span class="solar"></span>',
+    '\\[Filobscur\\]': '<span class="strand"></span>',
+    '\\[Chancellement\\]': '<span class="unstoppable"></span>',
+    '\\[Perforation de bouclier\\]': '<span class="barrier"></span>',
+    '\\[Perturbation\\]': '<span class="overload"></span>',
+    '\\[Stase\\]': '<span class="stasis"></span>',
+    '\\[Abyssal\\]': '<span class="void"></span>',
+    '\\[Cryo-électrique\\]': '<span class="arc"></span>',
+    '\\[Primaire\\]': '<span class="primary"></span>',
+    '\\[Spéciale\\]': '<span class="special"></span>',
+    '\\[Lourde\\]': '<span class="heavy"></span>',
+    '\\[PVE\\]': '<span class="pve"></span>',
+    '\\[PVP\\]': '<span class="pvp"></span>',
+    '\\[Chasseur\\]': '<span class="hunter"></span>',
+    '\\[Arcaniste\\]': '<span class="warlock"></span>',
+    '\\[Titan\\]': '<span class="titan"></span>'
+  };
+
+  for (const [key, value] of Object.entries(replacements)) {
+    const regex = new RegExp(key, 'g');
+    text = text.replace(regex, value);
+  }
+
+  return text;
+}
+
+function replaceVariablesInDescription(description) {
+  // Remplacer toutes les occurrences de {var:xxxxx} par "25"
+  const regex = /\{var:([a-zA-Z0-9_]+)\}/g; // Recherche du pattern {var:xxxxx}
+  return description.replace(regex, '25'); // Remplace par "25"
+}
+
+
 function openPopupItem(id, item) {
   const props = item.displayProperties;
 
   document.getElementById('popupitem-icon').src = "https://www.bungie.net" + props.icon;
   document.getElementById('popupitem-name').textContent = props.name;
-  document.getElementById('popupitem-description').textContent = props.description;
+  const modifiedDescription = replaceVariablesInDescription(props.description);
+  document.getElementById('popupitem-description').innerHTML = parseDescription(modifiedDescription);
   document.getElementById('popupitem-id').textContent = `ID: ${id}`;
 
   document.getElementById('popupitem').classList.add('show');
