79d25941ceb2360820697208417b4088f682f562
lsire <loan.sire@corys.fr>
2026-01-08 18:21:09 +0100

    Adding new tabs to the DDCVacuum

--- Scripts/ddcVacuum/utils/config.py
+++ Scripts/ddcVacuum/utils/config.py
@@ -2,11 +2,18 @@ SHEET_ID = "1tfa3mEwTWLrPUEw2p9aRKWUH37quVgpkyVisB6E1DZU"
 
 SHEETS = {
     "WeaponPerks": "1703329297",
-    "SeasonWeaponPerks": "296976005",
+    "SeasonWeaponPerks": "1175376862",
     "WeaponMods": "296976005",
     "IntrinsicTraits": "1368736324",
     "OriginTraits": "1050893845",
     "ArmorSets": "1916736284",
+
+    "ArcVerbs": "41303738",
+    "SolarVerbs": "993506142",
+    "VoidVerbs": "1094405333",
+    "StasisVerbs": "258124247",
+    "StrandVerbs": "976476629",
+    "PrismaticVerbs": "1280102267",
 }
 
 OUTPUT_DIR = "data"
